18a06c762d492adb5cb9793b62aa39b636a65e08
Mateen Azfar <mateenazfar786@gmail.com>
2025-12-25 10:59:38 +0530

    Mateen 10:59

--- src/App.jsx
+++ src/App.jsx
@@ -5,45 +5,7 @@ function App() {
 
   return (
     <div>
-      <h1>Assalamvalaikum This is Mateen</h1>
-      <p>Welcome to my React App Fazil</p>
-      <p>Hi Fazil How Are You</p>
-      <p>Hi Mateen How Are You This is Fazil</p>
-      <p>This Message Is Again From Fazil</p>
-      <p>This is Second Commit From Fazil</p>
-      <h1>Again This Message Is From Mateen</h1>
-      <p>........................................</p>
-      <p>This Is Again From Fazil</p>
-      <p>,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,</p>
-      <p>This is From Fazil ........</p>
-      <h1>This Is From Mateen</h1>
-      <p>on 6:45pm</p>
-
-      <p>This is From Fazil on 7:02</p>
-
-      <p>..................................................</p>
-      <p>This is From Mateeen ON 6:58</p>
-      <p>,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,</p>
-
-
-      <p>,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,</p>
-      <p>This Is From Fazil On 7:00pm</p>
-      <p>...............................................</p>
-
-      <p>This is From Feature Branch</p>
-      <p>This is Also From Feature Branch</p>
-
-
-      <p>,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,</p>
-      <h1>This Is From Amamed Pathan On 8:45</h1>
-
-      <h1>This is Also From Pathan on 8:55</h1>
-
-
-      <p>This is From Ahamed Khan On 9:15pm</p>
-      <h1>Today is the first day of Git Collab</h1>
-      
-      <h1>Second commit 9:19 PM</h1>
+      <h1>Again Starting By Mateen on 10:59</h1>
     </div>
   )
 }
